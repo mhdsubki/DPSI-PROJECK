32916c352e7573d47171532899f075f3a62eef97
mhdsubki <mhdsuki2@gmail.com>
2024-06-27 23:36:22 +0700

    first commit

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require("sequelize");
+
+// Konfigurasi koneksi Sequelize
+const sequelize = new Sequelize("dpsi", "root", "", {
+  // host: "127.0.0.1",
+  host: "localhost",
+  dialect: "mysql",
+  logging: false,
+});
+
+// Uji koneksi
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+// Ekspor instance sequelize untuk digunakan di tempat lain
+module.exports = sequelize;
